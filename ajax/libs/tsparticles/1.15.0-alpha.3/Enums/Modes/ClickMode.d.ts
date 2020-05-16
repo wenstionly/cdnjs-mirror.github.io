6acd16ae2bf3289747278eaca2f7109eae63741e
robocdnjs <cdnjs-github@cloudflare.com>
2020-05-16 23:13:34 +0000

    Add tsparticles v1.15.0-alpha.3

---
+++ ajax/libs/tsparticles/1.15.0-alpha.3/Enums/Modes/ClickMode.d.ts
@@ -0,0 +1,9 @@
+export declare enum ClickMode {
+    bubble = "bubble",
+    push = "push",
+    remove = "remove",
+    repulse = "repulse",
+    emitter = "emitter",
+    absorber = "absorber",
+    pause = "pause"
+}
