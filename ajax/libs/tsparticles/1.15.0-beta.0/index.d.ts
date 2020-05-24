a89360cf826976634d50513dd3971bd126b3e728
robocdnjs <cdnjs-github@cloudflare.com>
2020-05-24 01:45:48 +0000

    Add tsparticles v1.15.0-beta.0

---
+++ ajax/libs/tsparticles/1.15.0-beta.0/index.d.ts
@@ -0,0 +1,6 @@
+import { particlesJS, pJSDom, tsParticles } from "./index.slim";
+import "./Plugins/Absorbers/AbsorbersPlugin";
+import "./Plugins/Emitters/EmittersPlugin";
+import "./Plugins/PolygonMask/PolygonMaskPlugin";
+export { particlesJS, pJSDom, tsParticles };
+export * from "./Enums";
