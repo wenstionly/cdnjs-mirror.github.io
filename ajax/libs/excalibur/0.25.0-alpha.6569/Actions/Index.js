45971d622a9bdbe69bdb1b0113f1be3f677cea09
robocdnjs <cdnjs-github@cloudflare.com>
2020-05-25 19:06:45 +0000

    Add excalibur v0.25.0-alpha.6569

---
+++ ajax/libs/excalibur/0.25.0-alpha.6569/Actions/Index.js
@@ -0,0 +1,8 @@
+export * from './ActionContext';
+export * from './Actionable';
+export * from './RotationType';
+import * as actions from './Action';
+export { actions as Actions };
+// legacy Internal.Actions namespace support
+export var Internal = { Actions: actions };
+//# sourceMappingURL=Index.js.map
