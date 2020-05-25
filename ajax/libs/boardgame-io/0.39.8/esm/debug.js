7cee2d4d696d3211b901d5b30a9bc86fbabb2520
robocdnjs <cdnjs-github@cloudflare.com>
2020-05-25 11:37:06 +0000

    Add boardgame-io v0.39.8

---
+++ ajax/libs/boardgame-io/0.39.8/esm/debug.js
@@ -0,0 +1,5 @@
+import './reducer-4c0b6feb.js';
+import 'immer';
+export { D as Debug } from './Debug-2bb5239f.js';
+import 'flatted';
+import './ai-619ac716.js';
---
+++ ajax/libs/boardgame-io/0.39.8/esm/multiplayer.js
@@ -0,0 +1,8 @@
+import './reducer-4c0b6feb.js';
+import 'redux';
+import 'immer';
+import './initialize-0ff3bef6.js';
+import './base-c99f5be2.js';
+export { L as Local, S as SocketIO } from './socketio-4c77ab90.js';
+import './master-d8015b54.js';
+import 'socket.io-client';
