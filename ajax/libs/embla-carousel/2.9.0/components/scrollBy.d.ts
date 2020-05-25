8318e2b590d8b249a2b1014765e08b311a9a46fd
robocdnjs <cdnjs-github@cloudflare.com>
2020-05-25 13:46:07 +0000

    Add embla-carousel v2.9.0

---
+++ ajax/libs/embla-carousel/2.9.0/components/scrollBy.d.ts
@@ -0,0 +1,12 @@
+import { Limit } from './limit';
+import { Vector1D } from './vector1d';
+declare type Params = {
+    loop: boolean;
+    limit: Limit;
+    target: Vector1D;
+};
+export declare type ScrollBy = {
+    progress: (n: number) => number;
+};
+export declare function ScrollBy(params: Params): ScrollBy;
+export {};
---
+++ ajax/libs/embla-carousel/2.9.0/components/scrollTarget.d.ts
@@ -0,0 +1,23 @@
+import { Alignments } from './alignment';
+import { Counter } from './counter';
+import { Limit } from './limit';
+import { Vector1D } from './vector1d';
+declare type Params = {
+    align: Alignments;
+    index: Counter;
+    loop: boolean;
+    scrollSnaps: number[];
+    contentSize: number;
+    limit: Limit;
+    target: Vector1D;
+};
+export declare type Target = {
+    distance: number;
+    index: number;
+};
+export declare type ScrollTarget = {
+    byIndex: (target: number, direction: number) => Target;
+    byDistance: (force: number, snap: boolean) => Target;
+};
+export declare function ScrollTarget(params: Params): ScrollTarget;
+export {};
