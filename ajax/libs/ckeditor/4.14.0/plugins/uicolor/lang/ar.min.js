f25a88f83728e8d2ec1b028177fffdf46263445d
robocdnjs <cdnjs-github@cloudflare.com>
2020-05-25 12:27:41 +0000

    Add ckeditor v4.14.0

---
+++ ajax/libs/ckeditor/4.14.0/plugins/uicolor/lang/ar.min.js
@@ -0,0 +1,1 @@
+CKEDITOR.plugins.setLang("uicolor","ar",{title:"منتقي الألوان",options:"Color Options",highlight:"Highlight",selected:"Selected Color",predefined:"مجموعات ألوان معرفة مسبقا",config:"قص السطر إلى الملف config.js"});
---
+++ ajax/libs/ckeditor/4.14.0/plugins/uicolor/lang/da.min.js
@@ -0,0 +1,1 @@
+CKEDITOR.plugins.setLang("uicolor","da",{title:"Brugerflade på farvevælger",options:"Color Options",highlight:"Highlight",selected:"Selected Color",predefined:"Prædefinerede farveskemaer",config:"Indsæt denne streng i din config.js fil"});
