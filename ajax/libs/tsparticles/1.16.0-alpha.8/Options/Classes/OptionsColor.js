2c6af964d6273d175691022fd01cd37b5b5d718c
robocdnjs <cdnjs-github@cloudflare.com>
2020-06-08 22:02:40 +0000

    Add tsparticles v1.16.0-alpha.8

---
+++ ajax/libs/tsparticles/1.16.0-alpha.8/Options/Classes/OptionsColor.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OptionsColor = void 0;
+class OptionsColor {
+    constructor() {
+        this.value = "#fff";
+    }
+    static create(source, data) {
+        const color = source !== null && source !== void 0 ? source : new OptionsColor();
+        if (data !== undefined) {
+            color.load(typeof data === "string" ? { value: data } : data);
+        }
+        return color;
+    }
+    load(data) {
+        if (data !== undefined) {
+            if (data.value !== undefined) {
+                this.value = data.value;
+            }
+        }
+    }
+}
+exports.OptionsColor = OptionsColor;
