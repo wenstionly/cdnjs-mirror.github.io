780de058820d1de6937850f12d3111d3e7a0bebc
robocdnjs <cdnjs-github@cloudflare.com>
2020-06-11 19:54:45 +0000

    Add tsparticles v1.16.0-alpha.12

---
+++ ajax/libs/tsparticles/1.16.0-alpha.12/Options/Interfaces/Interactivity/Modes/IRepulseDiv.d.ts
@@ -0,0 +1,6 @@
+import type { IOptionLoader } from "../../IOptionLoader";
+import type { SingleOrMultiple } from "../../../../Types/SingleOrMultiple";
+import type { IRepulseBase } from "./IRepulseBase";
+export interface IRepulseDiv extends IOptionLoader<IRepulseDiv>, IRepulseBase {
+    ids: SingleOrMultiple<string>;
+}
