c20832192885e470efc1fe0426becace444eeb75
robocdnjs <cdnjs-github@cloudflare.com>
2020-06-21 10:50:30 +0000

    Add tsparticles v1.17.0-alpha.2

---
+++ ajax/libs/tsparticles/1.17.0-alpha.2/Core/Interfaces/IMouseData.d.ts
@@ -0,0 +1,8 @@
+import type { ICoordinates } from "./ICoordinates";
+export interface IMouseData {
+    clickPosition?: ICoordinates;
+    position?: ICoordinates;
+    clickTime?: number;
+    inside: boolean;
+    clicking: boolean;
+}
