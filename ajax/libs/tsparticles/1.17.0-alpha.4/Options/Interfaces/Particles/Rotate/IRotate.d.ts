c766c3618d82bf3899c4537021edbb8a8556eeca
robocdnjs <cdnjs-github@cloudflare.com>
2020-06-22 16:11:17 +0000

    Add tsparticles v1.17.0-alpha.4

---
+++ ajax/libs/tsparticles/1.17.0-alpha.4/Options/Interfaces/Particles/Rotate/IRotate.d.ts
@@ -0,0 +1,10 @@
+import type { IRotateAnimation } from "./IRotateAnimation";
+import type { IOptionLoader } from "../../IOptionLoader";
+import type { RotateDirection } from "../../../../Enums";
+export interface IRotate extends IOptionLoader<IRotate> {
+    animation: IRotateAnimation;
+    direction: RotateDirection;
+    path: boolean;
+    random: boolean;
+    value: number;
+}
