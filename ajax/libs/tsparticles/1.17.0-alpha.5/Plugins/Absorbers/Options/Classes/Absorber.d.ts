df311b4f89a245cd1194911ffc52e0f18d0afa71
robocdnjs <cdnjs-github@cloudflare.com>
2020-06-23 17:59:31 +0000

    Add tsparticles v1.17.0-alpha.5

---
+++ ajax/libs/tsparticles/1.17.0-alpha.5/Plugins/Absorbers/Options/Classes/Absorber.d.ts
@@ -0,0 +1,14 @@
+import type { IAbsorber } from "../Interfaces/IAbsorber";
+import type { ICoordinates } from "../../../../Core/Interfaces/ICoordinates";
+import type { RecursivePartial } from "../../../../Types/RecursivePartial";
+import { AbsorberSize } from "./AbsorberSize";
+import { OptionsColor } from "../../../../Options/Classes/OptionsColor";
+export declare class Absorber implements IAbsorber {
+    color: OptionsColor;
+    draggable: boolean;
+    opacity: number;
+    position?: ICoordinates;
+    size: AbsorberSize;
+    constructor();
+    load(data?: RecursivePartial<IAbsorber>): void;
+}
