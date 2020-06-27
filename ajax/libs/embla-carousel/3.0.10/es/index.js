9b4d21ea2d461569ed1c4fc3de8c0efca36c3451
robocdnjs <cdnjs-github@cloudflare.com>
2020-06-27 13:13:48 +0000

    Add embla-carousel v3.0.10

---
+++ ajax/libs/embla-carousel/3.0.10/es/index.js
@@ -0,0 +1,4 @@
+export { default } from './embla-carousel.js';
+import 'react';
+export { useEmblaCarousel } from './embla-carousel-react.js';
+//# sourceMappingURL=index.js.map
---
+++ ajax/libs/embla-carousel/3.0.10/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var emblaCarousel = require('./embla-carousel.js');
+require('react');
+var emblaCarouselReact = require('./embla-carousel-react.js');
+
+
+
+exports.default = emblaCarousel.default;
+exports.useEmblaCarousel = emblaCarouselReact.useEmblaCarousel;
+//# sourceMappingURL=index.js.map
